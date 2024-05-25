ca62332d18e5565a901226c6e3d61f4cd2ac2f72
Gordon Schulz <gordon@gordonschulz.de>
2024-05-25 09:55:32 +0200

    chore: Cleanup stuff set by lazyvim anyway

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,9 +5,6 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         sh = { "shellharden", "shfmt" },
-        hcl = { "packer_fmt" },
-        go = { "gofumpt" },
-        markdown = { "markdownlint" },
         python = { "isort", "black" },
       },
     }
--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -2,10 +2,9 @@ return {
   "mfussenegger/nvim-lint",
   opts = {
     linters_by_ft = {
-      lua = { "luacheck" },
       sh = { "shellcheck" },
       go = { "golangcilint", "revive" },
-      python = { "mypy", "pylint", "pycodestyle", "pydocstyle" },
+      python = { "mypy", "pylint", "pydocstyle" },
     },
   },
 }
