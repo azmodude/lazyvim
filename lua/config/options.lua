82a76ac557d7d41f51706f45a12601440d226a0e
Gordon Schulz <gordon@gordonschulz.de>
2024-05-27 20:30:51 +0200

    fix: Clipboard handling

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,6 @@
 
 vim.g.copilot_proxy = vim.env.HTTPS_PROXY or vim.env.HTTP_PROXY or vim.env.https_proxy or vim.env.http_proxy
 
-vim.opt.clipboard = "unnamed"
+vim.opt.clipboard = "unnamed,unnamedplus"
 vim.opt.showbreak = "↪"
 vim.opt.listchars = "tab:→ ,eol:↲,nbsp:␣,trail:•,extends:⟩,precedes:⟨"
