320e2f0c025d75f3a137a5e98188988be25da07c
Gordon Schulz <gordon.schulz@f-i.de>
2024-05-27 10:29:53 +0200

    feat: Enable codelens

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,8 @@
-if true then return {} end
-
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        marksman = {},
-      },
+  "neovim/nvim-lspconfig",
+  opts = {
+    codelens = {
+      enabled = true,
     },
   },
 }
