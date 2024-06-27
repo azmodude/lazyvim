8475a50eb8323fd4c9a783a06bfc9f42be4cc67f
Gordon Schulz <gordon.schulz@f-i.de>
2024-06-27 11:23:59 +0200

    feat: Remove shellharden from conform
    It does too much and tends to break some bash-isms

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
     opts.formatters_by_ft = opts.formatters_by_ft or {}
-    opts.formatters_by_ft["sh"] = { "shellharden", "shfmt" }
+    opts.formatters_by_ft["sh"] = { "shfmt" }
     opts.formatters_by_ft["python"] = { "isort", "black" }
   end,
 }
