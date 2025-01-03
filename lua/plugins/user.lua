c26eef89d46b11e0ed0d72b942a56f612adac838
Gordon Schulz <gordon@gordonschulz.de>
2025-01-03 12:32:48 +0100

    chore: Remove obsolete plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,17 +1,16 @@
 local internal = require("../custom/functions")
 
 return {
-  { "folke/zen-mode.nvim", lazy = true, cmd = { "ZenMode" } },
   { "hiphish/rainbow-delimiters.nvim", lazy = true, event = "BufReadPre" },
-  { "sindrets/diffview.nvim", lazy = true, cmd = { "DiffviewOpen", "DiffviewFileHistory" } },
+  { "sindrets/diffview.nvim",          lazy = true, cmd = { "DiffviewOpen", "DiffviewFileHistory" } },
   {
     "neogitorg/neogit",
     lazy = true,
     cmd = "Neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim", -- required
+      "nvim-lua/plenary.nvim",         -- required
       "nvim-telescope/telescope.nvim", -- optional
-      "sindrets/diffview.nvim", -- optional
+      "sindrets/diffview.nvim",        -- optional
     },
     config = true,
   },
@@ -63,39 +62,5 @@ return {
         disable_when_zoomed = false, -- defaults to false
       })
     end,
-  },
-  {
-    "serenevoid/kiwi.nvim",
-    lazy = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>cw", ':lua require("kiwi").open_wiki_index()<cr>', desc = "Open Wiki index" },
-      {
-        "<leader>wp",
-        ':lua require("kiwi").open_wiki_index("personal")<cr>',
-        desc = "Open index of personal wiki",
-      },
-      { "T", ':lua require("kiwi").todo.toggle()<cr>', desc = "Toggle Markdown Task" },
-    },
-  },
-  {
-    "akinsho/nvim-toggleterm.lua",
-    keys = "<C-`>",
-    event = "VeryLazy",
-    init = function()
-      -- Hide number column for
-      -- vim.cmd [[au TermOpen * setlocal nonumber norelativenumber]]
-
-      -- Esc twice to get to normal mode
-      vim.cmd([[tnoremap <ESC> <C-\><C-N>]])
-    end,
-    opts = {
-      size = 20,
-      open_mapping = [[<C-`>]],
-      shading_factor = 0.3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-      direction = "float",
-    },
   },
 }
